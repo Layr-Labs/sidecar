4d7e6f5638a64158128878cae534a2f8b9fe144d
0xrajath <rajathalex@gmail.com>
2024-11-19 15:57:48 -0500

    migration

--- pkg/postgres/migrations/migrator.go
+++ pkg/postgres/migrations/migrator.go
@@ -35,6 +35,7 @@ import (
 	_202411130953_addHashColumns "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411130953_addHashColumns"
 	_202411131200_eigenStateModelConstraints "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411131200_eigenStateModelConstraints"
 	_202411151931_operatorDirectedRewardSubmissions "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411151931_operatorDirectedRewardSubmissions"
+	_202411191550_operatorAVSSplits "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411191550_operatorAVSSplits"
 	_202411191947_cleanupUnusedTables "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411191947_cleanupUnusedTables"
 
 	"go.uber.org/zap"
@@ -107,6 +108,7 @@ func (m *Migrator) MigrateAll() error {
 		&_202411130953_addHashColumns.Migration{},
 		&_202411131200_eigenStateModelConstraints.Migration{},
 		&_202411151931_operatorDirectedRewardSubmissions.Migration{},
+		&_202411191550_operatorAVSSplits.Migration{},
 		&_202411191947_cleanupUnusedTables.Migration{},
 	}
 
