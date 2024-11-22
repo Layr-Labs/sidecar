bb003cd9ba0eeb75f5d137b46f321d2c2ac94318
0xrajath <rajathalex@gmail.com>
2024-11-22 13:42:55 -0500

    migration

--- pkg/postgres/migrations/migrator.go
+++ pkg/postgres/migrations/migrator.go
@@ -38,6 +38,8 @@ import (
 	_202411191550_operatorAVSSplits "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411191550_operatorAVSSplits"
 	_202411191708_operatorPISplits "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411191708_operatorPISplits"
 	_202411191947_cleanupUnusedTables "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411191947_cleanupUnusedTables"
+	_202411221331_operatorAVSSplitSnapshots "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411221331_operatorAVSSplitSnapshots"
+	_202411221331_operatorPISplitSnapshots "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202411221331_operatorPISplitSnapshots"
 
 	"go.uber.org/zap"
 	"gorm.io/gorm"
@@ -112,6 +114,8 @@ func (m *Migrator) MigrateAll() error {
 		&_202411191550_operatorAVSSplits.Migration{},
 		&_202411191708_operatorPISplits.Migration{},
 		&_202411191947_cleanupUnusedTables.Migration{},
+		&_202411221331_operatorAVSSplitSnapshots.Migration{},
+		&_202411221331_operatorPISplitSnapshots.Migration{},
 	}
 
 	for _, migration := range migrations {
