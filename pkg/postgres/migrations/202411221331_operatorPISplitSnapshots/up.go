5e6be5642bf9e6c5fc5ddc83b39d35bfa3e2c79c
0xrajath <rajathalex@gmail.com>
2024-11-22 13:42:55 -0500

    migration

---
+++ pkg/postgres/migrations/202411221331_operatorPISplitSnapshots/up.go
@@ -0,0 +1,30 @@
+package _202411221331_operatorPISplitSnapshots
+
+import (
+	"database/sql"
+
+	"gorm.io/gorm"
+)
+
+type Migration struct {
+}
+
+func (m *Migration) Up(db *sql.DB, grm *gorm.DB) error {
+	queries := []string{
+		`CREATE TABLE IF NOT EXISTS operator_pi_split_snapshots (
+			operator varchar not null,
+			split integer not null,
+			snapshot date not null
+		)`,
+	}
+	for _, query := range queries {
+		if _, err := db.Exec(query); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (m *Migration) GetName() string {
+	return "202411221331_operatorPISplitSnapshots"
+}
