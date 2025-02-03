b98c0c977bccd11aab6c7a236b55d5b1057ec448
0xrajath <rajathalex@gmail.com>
2025-02-03 17:41:01 -0500

    perf: remove unnecssary cte

--- pkg/rewards/operatorDirectedOperatorSetRewards.go
+++ pkg/rewards/operatorDirectedOperatorSetRewards.go
@@ -3,46 +3,26 @@ package rewards
 import "github.com/Layr-Labs/sidecar/pkg/rewardsUtils"
 
 const operatorDirectedOperatorSetRewardsQuery = `
-	with _operator_directed_operator_set_rewards as (
-		SELECT
-			odosrs.avs,
-			odosrs.operator_set_id,
-			odosrs.reward_hash,
-			odosrs.token,
-			odosrs.operator,
-			odosrs.operator_index,
-			odosrs.amount,
-			odosrs.strategy,
-			odosrs.strategy_index,
-			odosrs.multiplier,
-			odosrs.start_timestamp::TIMESTAMP(6),
-			odosrs.end_timestamp::TIMESTAMP(6),
-			odosrs.duration,
-			odosrs.block_number,
-			b.block_time::TIMESTAMP(6),
-			TO_CHAR(b.block_time, 'YYYY-MM-DD') AS block_date
-		FROM operator_directed_operator_set_reward_submissions AS odosrs
-		JOIN blocks AS b ON (b.number = odosrs.block_number)
-		WHERE b.block_time < TIMESTAMP '{{.cutoffDate}}'
-	)
-	select
-		avs,
-		operator_set_id,
-		reward_hash,
-		token,
-		operator,
-		operator_index,
-		amount,
-		strategy,
-		strategy_index,
-		multiplier,
-		start_timestamp::TIMESTAMP(6),
-		end_timestamp::TIMESTAMP(6),
-		duration,
-		block_number,
-		block_time,
-		block_date
-	from _operator_directed_operator_set_rewards
+SELECT
+	odosrs.avs,
+	odosrs.operator_set_id,
+	odosrs.reward_hash,
+	odosrs.token,
+	odosrs.operator,
+	odosrs.operator_index,
+	odosrs.amount,
+	odosrs.strategy,
+	odosrs.strategy_index,
+	odosrs.multiplier,
+	odosrs.start_timestamp::TIMESTAMP(6),
+	odosrs.end_timestamp::TIMESTAMP(6),
+	odosrs.duration,
+	odosrs.block_number,
+	b.block_time::TIMESTAMP(6),
+	TO_CHAR(b.block_time, 'YYYY-MM-DD') AS block_date
+FROM operator_directed_operator_set_reward_submissions AS odosrs
+JOIN blocks AS b ON (b.number = odosrs.block_number)
+WHERE b.block_time < TIMESTAMP '{{.cutoffDate}}'
 `
 
 func (r *RewardsCalculator) GenerateAndInsertOperatorDirectedOperatorSetRewards(snapshotDate string) error {
