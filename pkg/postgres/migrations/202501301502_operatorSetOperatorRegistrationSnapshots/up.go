f495299714387410fb2454c043a384272a515111
0xrajath <rajathalex@gmail.com>
2025-02-03 17:11:54 -0500

    fix: unique constraint

--- pkg/postgres/migrations/202501301502_operatorSetOperatorRegistrationSnapshots/up.go
+++ pkg/postgres/migrations/202501301502_operatorSetOperatorRegistrationSnapshots/up.go
@@ -16,7 +16,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
 			operator varchar not null,
 			avs varchar not null,
 			operator_set_id bigint not null,
-			snapshot date not null
+			snapshot date not null,
+			UNIQUE (operator, avs, operator_set_id, snapshot)
 		)`,
 	}
 	for _, query := range queries {
