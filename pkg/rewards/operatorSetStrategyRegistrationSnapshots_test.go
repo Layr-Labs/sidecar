23763f761c858d53440b17958ee029eaa0e21290
Sean McGary <sean@seanmcgary.com>
2025-02-19 21:41:11 -0600

    chore: update tests with changes due to new features after rebasing

--- pkg/rewards/operatorSetStrategyRegistrationSnapshots_test.go
+++ pkg/rewards/operatorSetStrategyRegistrationSnapshots_test.go
@@ -2,6 +2,7 @@ package rewards
 
 import (
 	"fmt"
+	"github.com/Layr-Labs/sidecar/internal/metrics"
 	"testing"
 
 	"github.com/Layr-Labs/sidecar/internal/config"
@@ -20,6 +21,7 @@ func setupOperatorSetStrategyRegistrationSnapshot() (
 	*config.Config,
 	*gorm.DB,
 	*zap.Logger,
+	*metrics.MetricsSink,
 	error,
 ) {
 	testContext := getRewardsTestContext()
@@ -32,19 +34,20 @@ func setupOperatorSetStrategyRegistrationSnapshot() (
 	case "mainnet-reduced":
 		cfg.Chain = config.Chain_Mainnet
 	default:
-		return "", nil, nil, nil, fmt.Errorf("Unknown test context")
+		return "", nil, nil, nil, nil, fmt.Errorf("Unknown test context")
 	}
 
 	cfg.DatabaseConfig = *tests.GetDbConfigFromEnv()
 
 	l, _ := logger.NewLogger(&logger.LoggerConfig{Debug: cfg.Debug})
+	sink, _ := metrics.NewMetricsSink(&metrics.MetricsSinkConfig{}, nil)
 
 	dbname, _, grm, err := postgres.GetTestPostgresDatabase(cfg.DatabaseConfig, cfg, l)
 	if err != nil {
-		return dbname, nil, nil, nil, err
+		return dbname, nil, nil, nil, nil, err
 	}
 
-	return dbname, cfg, grm, l, nil
+	return dbname, cfg, grm, l, sink, nil
 }
 
 func teardownOperatorSetStrategyRegistrationSnapshot(dbname string, cfg *config.Config, db *gorm.DB, l *zap.Logger) {
@@ -84,7 +87,7 @@ func Test_OperatorSetStrategyRegistrationSnapshots(t *testing.T) {
 	}
 
 	// projectRoot := getProjectRootPath()
-	dbFileName, cfg, grm, l, err := setupOperatorSetStrategyRegistrationSnapshot()
+	dbFileName, cfg, grm, l, sink, err := setupOperatorSetStrategyRegistrationSnapshot()
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -115,7 +118,7 @@ func Test_OperatorSetStrategyRegistrationSnapshots(t *testing.T) {
 
 	t.Run("Should generate the proper operatorSetStrategyRegistrationSnapshots", func(t *testing.T) {
 		sog := stakerOperators.NewStakerOperatorGenerator(grm, l, cfg)
-		rewards, _ := NewRewardsCalculator(cfg, grm, nil, sog, l)
+		rewards, _ := NewRewardsCalculator(cfg, grm, nil, sog, sink, l)
 
 		err := rewards.GenerateAndInsertOperatorSetStrategyRegistrationSnapshots(snapshotDate)
 		assert.Nil(t, err)
