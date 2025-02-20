73a852a66e5c85cfcb6e935b5e58cea62e01bd3b
Sean McGary <sean@seanmcgary.com>
2025-02-20 09:40:39 -0600

    stakerShares should look at past blocks (exclusive) when finding delegations

--- pkg/eigenState/stakerShares/stakerShares.go
+++ pkg/eigenState/stakerShares/stakerShares.go
@@ -731,7 +731,7 @@ func (ss *StakerSharesModel) GetDelegatedStakerSharesAtTimeOfSlashing(slashDiff 
 				ROW_NUMBER() OVER (PARTITION BY staker ORDER BY block_number desc, log_index desc) as rn
 			from staker_delegation_changes
 			where
-				block_number <= @blockNumber
+				block_number < @blockNumber
 		),
 		delegated_stakers as (
 			select
@@ -751,7 +751,7 @@ func (ss *StakerSharesModel) GetDelegatedStakerSharesAtTimeOfSlashing(slashDiff 
 			staker_share_deltas as ssd
 			on ssd.staker = ds.staker
 			and ssd.strategy = @strategy
-			and ssd.block_number <= @blockNumber
+			and ssd.block_number < @blockNumber
 		group by
 			ds.staker
 	`
