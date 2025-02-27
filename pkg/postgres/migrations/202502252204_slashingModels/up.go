feb5533212ac6acc639ff5fb965c216cf14a61d3
Sean McGary <sean@seanmcgary.com>
2025-02-27 11:17:49 -0600

    feat: add EigenState model for OperatorAllocationDelay

--- pkg/postgres/migrations/202502252204_slashingModels/up.go
+++ pkg/postgres/migrations/202502252204_slashingModels/up.go
@@ -37,7 +37,19 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
     	)`,
 		`create index if not exists idx_operator_allocations_operator on operator_allocations (operator)`,
 		`create index if not exists idx_operator_allocations_avs on operator_allocations (avs)`,
-		`create index if not exists idx_operator_allocations_number on operator_sets (block_number)`,
+		`create index if not exists idx_operator_allocations_block_number on operator_sets (block_number)`,
+		// allocation delays
+		`CREATE TABLE IF NOT EXISTS operator_allocation_delays (
+    		operator varchar not null,
+    		effective_block bigint not null,
+    		delay bigint not null,
+			block_number bigint not null,
+			transaction_hash varchar not null,
+			log_index bigint not null,
+			unique (transaction_hash, log_index, block_number)
+    	)`,
+		`create index if not exists idx_operator_allocation_delays_operator on operator_allocations (operator)`,
+		`create index if not exists idx_operator_allocation_delays_block_number on operator_sets (block_number)`,
 	}
 	for _, query := range queries {
 		res := grm.Exec(query)
