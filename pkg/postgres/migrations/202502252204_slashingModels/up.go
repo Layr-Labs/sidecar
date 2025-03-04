db8a6158ebc5c2a036b0082e2e7afda8b4e13ddf
Sean McGary <sean@seanmcgary.com>
2025-03-04 10:29:06 -0600

    add proper block number foreign key relationships

--- pkg/postgres/migrations/202502252204_slashingModels/up.go
+++ pkg/postgres/migrations/202502252204_slashingModels/up.go
@@ -18,7 +18,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
 			block_number bigint not null,
 			transaction_hash varchar not null,
 			log_index bigint not null,
-			constraint uniq_operator_sets unique(transaction_hash, log_index, block_number)
+			constraint uniq_operator_sets unique(transaction_hash, log_index, block_number),
+    		constraint operator_sets_block_number_fkey foreign key (block_number) references blocks(number) on delete cascade
     	)`,
 		`create index if not exists idx_operator_sets_avs on operator_sets (avs)`,
 		`create index if not exists idx_operator_sets_block_number on operator_sets (block_number)`,
@@ -33,7 +34,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
 			block_number bigint not null,
 			transaction_hash varchar not null,
 			log_index bigint not null,
-			constraint uniq_operator_allocations unique(transaction_hash, log_index, block_number)
+			constraint uniq_operator_allocations unique(transaction_hash, log_index, block_number),
+    		constraint operator_allocations_block_number_fkey foreign key (block_number) references blocks(number) on delete cascade
     	)`,
 		`create index if not exists idx_operator_allocations_operator on operator_allocations (operator)`,
 		`create index if not exists idx_operator_allocations_avs on operator_allocations (avs)`,
@@ -46,7 +48,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
 			block_number bigint not null,
 			transaction_hash varchar not null,
 			log_index bigint not null,
-			constraint uniq_operator_allocation_delays unique(transaction_hash, log_index, block_number)
+			constraint uniq_operator_allocation_delays unique(transaction_hash, log_index, block_number),
+    		constraint operator_allocation_delays_block_number_fkey foreign key (block_number) references blocks(number) on delete cascade
     	)`,
 		`create index if not exists idx_operator_allocation_delays_operator on operator_allocations (operator)`,
 		`create index if not exists idx_operator_allocation_delays_block_number on operator_sets (block_number)`,
@@ -61,7 +64,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
     		block_number bigint not null,
     		transaction_hash varchar not null,
     		log_index bigint not null,
-    		constraint uniq_slashed_operators unique(transaction_hash, log_index, block_number, operator, strategy, avs, operator_set_id)
+    		constraint uniq_slashed_operators unique(transaction_hash, log_index, block_number, operator, strategy, avs, operator_set_id),
+    		constraint slashed_operators_block_number_fkey foreign key (block_number) references blocks(number) on delete cascade
     	)`,
 		`create index if not exists idx_slashed_operators_operator on slashed_operators (operator)`,
 		`create index if not exists idx_slashed_operators_operator_avs on slashed_operators (operator, avs)`,
@@ -75,7 +79,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
     		transaction_hash varchar not null,
     		log_index bigint not null,
     		block_number bigint not null,
-    		constraint uniq_encumbered_magnitudes unique(transaction_hash, log_index, block_number)
+    		constraint uniq_encumbered_magnitudes unique(transaction_hash, log_index, block_number),
+    		constraint encumbered_magnitudes_block_number_fkey foreign key (block_number) references blocks(number) on delete cascade
     	)`,
 		`create index if not exists idx_encumbered_magnitudes_operator on encumbered_magnitudes (operator)`,
 		`create index if not exists idx_encumbered_magnitudes_operator_strategy on encumbered_magnitudes (operator, strategy)`,
@@ -88,7 +93,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
     		transaction_hash varchar not null,
     		log_index bigint not null,
     		block_number bigint not null,
-    		constraint uniq_operator_max_magnitudes unique(transaction_hash, log_index, block_number)
+    		constraint uniq_operator_max_magnitudes unique(transaction_hash, log_index, block_number),
+    		constraint operator_max_magnitudes_block_number_fkey foreign key (block_number) references blocks(number) on delete cascade
     	)`,
 		`create index if not exists idx_operator_max_magnitudes_operator on operator_max_magnitudes (operator)`,
 		`create index if not exists idx_operator_max_magnitudes_operator_strategy on operator_max_magnitudes (operator, strategy)`,
@@ -101,7 +107,8 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
     		transaction_hash varchar not null,
     		log_index bigint not null,
     		block_number bigint not null,
-    		constraint uniq_slashed_operator_shares unique(transaction_hash, log_index, block_number)
+    		constraint uniq_slashed_operator_shares unique(transaction_hash, log_index, block_number),
+    		constraint slashed_operator_shares_block_number_fkey foreign key (block_number) references blocks(number) on delete cascade
     	)`,
 		`create index if not exists idx_slashed_operator_shares_operator on slashed_operator_shares (operator)`,
 		`create index if not exists idx_slashed_operator_shares_operator_strategy on slashed_operator_shares (operator, strategy)`,
