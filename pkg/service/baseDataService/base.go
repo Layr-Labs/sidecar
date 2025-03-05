8bff756ea6837b4dec4a1cb17463d452f86d628a
Sean McGary <sean@seanmcgary.com>
2025-03-04 21:45:37 -0600

    feat: implement slashing related RPCs

--- pkg/service/baseDataService/base.go
+++ pkg/service/baseDataService/base.go
@@ -2,6 +2,9 @@ package baseDataService
 
 import (
 	"context"
+	"errors"
+	"fmt"
+	"github.com/Layr-Labs/sidecar/pkg/eigenState/stateManager"
 	"github.com/Layr-Labs/sidecar/pkg/storage"
 	"gorm.io/gorm"
 )
@@ -20,4 +23,29 @@ func (b *BaseDataService) GetCurrentBlockHeightIfNotPresent(ctx context.Context,
 		blockHeight = currentBlock.Number
 	}
 	return blockHeight, nil
+}
+
+func (b *BaseDataService) GetBlock(ctx context.Context, blockHeight uint64) (*storage.Block, error) {
+	var block *storage.Block
+	res := b.DB.Model(&storage.Block{}).Where("number = ?", blockHeight).First(&block)
+	if res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, res.Error
+	}
+	return block, nil
+}
+
+func (b *BaseDataService) GetLatestConfirmedBlock(ctx context.Context) (*storage.Block, error) {
+	var stateRoot *stateManager.StateRoot
+	res := b.DB.Model(&stateManager.StateRoot{}).Order("eth_block_number desc").First(&stateRoot)
+	if res.Error != nil {
+		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
+			return nil, fmt.Errorf("no state root found")
+		}
+		return nil, res.Error
+	}
+
+	return b.GetBlock(ctx, stateRoot.EthBlockNumber)
 }
