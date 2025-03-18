5f398ba224c91e9dfd60759f04dfc26b201aaaf7
Sean McGary <sean@seanmcgary.com>
2025-03-18 12:52:26 -0500

    update operator_directed_operator_set_rewards table insertion
    update more operatorSet tables

--- pkg/rewards/operatorDirectedOperatorSetRewards.go
+++ pkg/rewards/operatorDirectedOperatorSetRewards.go
@@ -1,8 +1,12 @@
 package rewards
 
-import "github.com/Layr-Labs/sidecar/pkg/rewardsUtils"
+import (
+	"github.com/Layr-Labs/sidecar/pkg/rewardsUtils"
+	"go.uber.org/zap"
+)
 
 const operatorDirectedOperatorSetRewardsQuery = `
+insert into operator_directed_operator_set_rewards (avs, operator_set_id, reward_hash, token, operator, operator_index, amount, strategy, strategy_index, multiplier, start_timestamp, end_timestamp, duration, block_number, block_time, block_date)
 SELECT
 	odosrs.avs,
 	odosrs.operator_set_id,
@@ -23,23 +27,25 @@ SELECT
 FROM operator_directed_operator_set_reward_submissions AS odosrs
 JOIN blocks AS b ON (b.number = odosrs.block_number)
 WHERE b.block_time < TIMESTAMP '{{.cutoffDate}}'
+on conflict on constraint uniq_operator_directed_operator_set_rewards do nothing;
 `
 
 func (r *RewardsCalculator) GenerateAndInsertOperatorDirectedOperatorSetRewards(snapshotDate string) error {
-	tableName := "operator_directed_operator_set_rewards"
-
 	query, err := rewardsUtils.RenderQueryTemplate(operatorDirectedOperatorSetRewardsQuery, map[string]interface{}{
 		"cutoffDate": snapshotDate,
 	})
 	if err != nil {
-		r.logger.Sugar().Errorw("Failed to render rewards combined query", "error", err)
+		r.logger.Sugar().Errorw("Failed to render operator_directed_operator_set_rewards query", "error", err)
 		return err
 	}
 
-	err = r.generateAndInsertFromQuery(tableName, query, nil)
-	if err != nil {
-		r.logger.Sugar().Errorw("Failed to generate combined rewards", "error", err)
-		return err
+	res := r.grm.Exec(query)
+	if res.Error != nil {
+		r.logger.Sugar().Errorw("Failed to generate operator_directed_operator_set_rewards",
+			zap.Error(res.Error),
+			zap.String("snapshotDate", snapshotDate),
+		)
+		return res.Error
 	}
 	return nil
 }
