be2bde63cae7500628a80e45e49ddf14c1e4d2e8
Sean McGary <sean@seanmcgary.com>
2025-03-18 13:06:00 -0500

    update more operatorSet tables

--- pkg/postgres/migrations/202503181244_snapshotUniqueConstraintsPartTwo/up.go
+++ pkg/postgres/migrations/202503181244_snapshotUniqueConstraintsPartTwo/up.go
@@ -14,6 +14,15 @@ func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
 	queries := []string{
 		`truncate table operator_directed_operator_set_rewards cascade`,
 		`alter table operator_directed_operator_set_rewards add constraint uniq_operator_directed_operator_set_rewards unique (block_number, reward_hash, strategy_index, operator_index)`,
+
+		`truncate table operator_set_operator_registration_snapshots cascade`,
+		`alter table operator_set_operator_registration_snapshots add constraint uniq_operator_set_operator_registration_snapshots unique (avs, operator, operator_set_id, snapshot)`,
+
+		`truncate table operator_set_split_snapshots cascade`,
+		`alter table operator_set_split_snapshots add constraint uniq_operator_set_split_snapshots unique (operator, avs, operator_set_id, snapshot)`,
+
+		`truncate table operator_set_strategy_registration_snapshots cascade`,
+		`alter table operator_set_strategy_registration_snapshots add constraint uniq_operator_set_strategy_registration_snapshots unique (avs, operator_set_id, strategy, snapshot)`,
 	}
 
 	for _, query := range queries {
