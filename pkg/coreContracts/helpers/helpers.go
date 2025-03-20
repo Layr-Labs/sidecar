8b637efa19f3c68705b47577829e4d46505be95e
Sean McGary <sean@seanmcgary.com>
2025-03-19 22:09:35 -0500

    add documentation

--- pkg/coreContracts/helpers/helpers.go
+++ pkg/coreContracts/helpers/helpers.go
@@ -1,3 +1,4 @@
+// Package helpers provides utility functions for importing and managing core contracts.
 package helpers
 
 import (
@@ -5,32 +6,55 @@ import (
 	"strings"
 )
 
+// ContractImport represents a contract to be imported into the system.
 type ContractImport struct {
+	// Address is the Ethereum address of the contract
 	Address      string
+	
+	// Abi is the Application Binary Interface (ABI) of the contract
 	Abi          string
+	
+	// BytecodeHash is the hash of the contract's bytecode
 	BytecodeHash string
 }
 
+// ImplementationContract represents an implementation contract for a proxy.
 type ImplementationContract struct {
-	// The implementation to create
+	// Contract is the implementation contract to create
 	Contract ContractImport
 
-	// the block number at which it was created
+	// BlockNumber is the block number at which the implementation was created
 	BlockNumber uint64
 }
 
+// ImplementationContractImport represents a set of implementation contracts
+// associated with a proxy contract.
 type ImplementationContractImport struct {
-	// top-level proxy to associate the implementation contract with
+	// ProxyContractAddress is the address of the top-level proxy contract
 	ProxyContractAddress string
 
+	// ImplementationContracts is a list of implementation contracts for the proxy
 	ImplementationContracts []*ImplementationContract
 }
 
+// ContractsToImport represents a collection of contracts to be imported into the system.
 type ContractsToImport struct {
+	// CoreContracts is a list of top-level core contracts to import
 	CoreContracts           []*ContractImport
+	
+	// ImplementationContracts is a list of implementation contracts and their proxies
 	ImplementationContracts []*ImplementationContractImport
 }
 
+// ImportCoreContracts imports a list of core contracts into the contract store.
+//
+// Parameters:
+//   - coreContracts: A list of core contracts to import
+//   - cs: The contract store to import into
+//
+// Returns:
+//   - []string: A list of addresses of contracts that were created
+//   - error: Any error that occurred during import
 func ImportCoreContracts(coreContracts []*ContractImport, cs contractStore.ContractStore) ([]string, error) {
 	createdContracts := make([]string, 0)
 	for _, c := range coreContracts {
@@ -54,6 +78,16 @@ func ImportCoreContracts(coreContracts []*ContractImport, cs contractStore.Contr
 	return createdContracts, nil
 }
 
+// ImportImplementationContracts imports a list of implementation contracts and associates them
+// with their respective proxy contracts.
+//
+// Parameters:
+//   - implementationContracts: A list of implementation contracts to import
+//   - cs: The contract store to import into
+//
+// Returns:
+//   - []string: A list of addresses of implementation contracts that were imported
+//   - error: Any error that occurred during import
 func ImportImplementationContracts(
 	implementationContracts []*ImplementationContractImport,
 	cs contractStore.ContractStore,
