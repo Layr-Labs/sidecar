98570786b8523285baf6a7e7c4d1a3d25d72a4f0
Sean McGary <sean@seanmcgary.com>
2025-03-19 21:50:46 -0500

    implement migration

--- pkg/coreContracts/types/types.go
+++ pkg/coreContracts/types/types.go
@@ -2,7 +2,7 @@ package types
 
 import (
 	"github.com/Layr-Labs/sidecar/internal/config"
-	"github.com/Layr-Labs/sidecar/pkg/contractManager"
+	"github.com/Layr-Labs/sidecar/pkg/contractStore"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
 )
@@ -10,12 +10,13 @@ import (
 type ICoreContractMigration interface {
 	GetName() string
 	// returns any new top-level core contracts that were added
-	Up(db *gorm.DB, cm contractManager.ContractManager, l *zap.Logger, cfg *config.Config) (*MigrationResult, error)
+	Up(db *gorm.DB, cs contractStore.ContractStore, l *zap.Logger, cfg *config.Config) (*MigrationResult, error)
 }
 
 type CoreContractAdded struct {
 	Address         string
 	IndexStartBlock uint64
+	Backfill        bool
 }
 
 type MigrationResult struct {
