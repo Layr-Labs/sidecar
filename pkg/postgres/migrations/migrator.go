ea0793ba40080bd1492f444d888604232fc4b202
Sean McGary <sean@seanmcgary.com>
2025-05-08 13:37:20 -0500

    rename migrations, add missing tables

--- pkg/postgres/migrations/migrator.go
+++ pkg/postgres/migrations/migrator.go
@@ -3,6 +3,8 @@ package migrations
 import (
 	"database/sql"
 	"fmt"
+	"time"
+
 	"github.com/Layr-Labs/sidecar/internal/config"
 	_202409061249_bootstrapDb "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202409061249_bootstrapDb"
 	_202409061250_eigenlayerStateTables "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202409061250_eigenlayerStateTables"
@@ -62,7 +64,6 @@ import (
 	_202502211539_hydrateClaimedRewards "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202502211539_hydrateClaimedRewards"
 	_202502252204_slashingModels "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202502252204_slashingModels"
 	_202503030846_cleanupConstraintNames "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202503030846_cleanupConstraintNames"
-	_202503041615_migrateRewardsTables "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202503041615_migrateRewardsTables"
 	_202503042014_stakerOperatorIndex "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202503042014_stakerOperatorIndex"
 	_202503051449_addContractTypeColumn "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202503051449_addContractTypeColumn"
 	_202503061009_pectraPrune "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202503061009_pectraPrune"
@@ -71,10 +72,10 @@ import (
 	_202503171414_slashingWithdrawals "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202503171414_slashingWithdrawals"
 	_202503311108_goldRewardHashIndex "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202503311108_goldRewardHashIndex"
 	_202504240743_fixQueuedSlashingWithdrawalsPk "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202504240743_fixQueuedSlashingWithdrawalsPk"
+	_202505081218_migrateRewardsTables "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202505081218_migrateRewardsTables"
 	_202505092007_startupJobs "github.com/Layr-Labs/sidecar/pkg/postgres/migrations/202505092007_startupJobs"
 	"go.uber.org/zap"
 	"gorm.io/gorm"
-	"time"
 )
 
 // Migration interface defines the contract for database migrations.
@@ -214,7 +215,7 @@ func (m *Migrator) MigrateAll() error {
 		&_202503171414_slashingWithdrawals.Migration{},
 		&_202504240743_fixQueuedSlashingWithdrawalsPk.Migration{},
 		&_202505092007_startupJobs.Migration{},
-		&_202503041615_migrateRewardsTables.Migration{},
+		&_202505081218_migrateRewardsTables.Migration{},
 	}
 
 	for _, migration := range migrations {
