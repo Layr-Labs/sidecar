b559300ef9a4e0021b90f5832664ca3446611f41
Sean McGary <sean@seanmcgary.com>
2025-06-12 08:03:45 -0500

    fix column

--- pkg/postgres/migrations/202503181244_snapshotUniqueConstraintsPartTwo/up.go
+++ pkg/postgres/migrations/202503181244_snapshotUniqueConstraintsPartTwo/up.go
@@ -13,6 +13,7 @@ type Migration struct {
 func (m *Migration) Up(db *sql.DB, grm *gorm.DB, cfg *config.Config) error {
 	queries := []string{
 		`truncate table operator_directed_operator_set_rewards cascade`,
+		`alter table operator_directed_operator_set_rewards alter column block_date type text`,
 		`alter table operator_directed_operator_set_rewards add constraint uniq_operator_directed_operator_set_rewards unique (block_number, reward_hash, strategy_index, operator_index)`,
 
 		`truncate table operator_set_operator_registration_snapshots cascade`,
