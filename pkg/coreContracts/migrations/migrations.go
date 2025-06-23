6682cd143a204730851be7845b3dc578c5cba91e
Seri Choi <choi.seri65@gmail.com>
2025-06-23 14:45:49 -0400

    abi into constants

--- pkg/coreContracts/migrations/migrations.go
+++ pkg/coreContracts/migrations/migrations.go
@@ -1,14 +1,14 @@
 package migrations
 
 import (
-	_202503191547_initializeAllNewContracts "github.com/Layr-Labs/sidecar/pkg/coreContracts/migrations/202503191547_initializeAllNewContracts"
+	_02503191547_initializeAllNewContracts "github.com/Layr-Labs/sidecar/pkg/coreContracts/migrations/202503191547_initializeAllNewContracts"
 	_202506181424_updateSepoliaAddresses "github.com/Layr-Labs/sidecar/pkg/coreContracts/migrations/202506181424_updateSepoliaAddresses"
 	"github.com/Layr-Labs/sidecar/pkg/coreContracts/types"
 )
 
 func GetCoreContractMigrations() []types.ICoreContractMigration {
 	return []types.ICoreContractMigration{
-		&_202503191547_initializeAllNewContracts.ContractMigration{},
+		&_02503191547_initializeAllNewContracts.ContractMigration{},
 		&_202506181424_updateSepoliaAddresses.ContractMigration{},
 	}
 }
