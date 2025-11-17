ce429e1b77bd8558af0b266bb319aa114c161f93
Seri Choi <choi.seri65@gmail.com>
2025-11-17 13:50:02 -0500

    update fork dates

--- internal/config/config.go
+++ internal/config/config.go
@@ -556,10 +556,6 @@ func (c *Config) GetRewardsSqlForkDates() (ForkMap, error) {
 				Date:        "2025-05-14",
 				BlockNumber: 3840004,
 			},
-			RewardsFork_Sabine: Fork{
-				Date:        "2025-11-18", // TODO: Set actual fork date
-				BlockNumber: 0,            // TODO: Set actual fork block number
-			},
 		}, nil
 	case Chain_Holesky:
 		return ForkMap{
@@ -598,10 +594,6 @@ func (c *Config) GetRewardsSqlForkDates() (ForkMap, error) {
 				Date:        "2025-05-14",
 				BlockNumber: 3840004,
 			},
-			RewardsFork_Sabine: Fork{
-				Date:        "2025-11-18", // TODO: Set actual fork date
-				BlockNumber: 0,            // TODO: Set actual fork block number
-			},
 		}, nil
 	case Chain_Sepolia:
 		return ForkMap{
@@ -631,8 +623,8 @@ func (c *Config) GetRewardsSqlForkDates() (ForkMap, error) {
 				BlockNumber: 8327038,
 			},
 			RewardsFork_Sabine: Fork{
-				Date:        "1970-01-01", // Enabled from genesis on Sepolia
-				BlockNumber: 0,
+				Date:        "2025-12-19",
+				BlockNumber: 9903838,
 			},
 		}, nil
 	case Chain_Hoodi:
@@ -663,8 +655,8 @@ func (c *Config) GetRewardsSqlForkDates() (ForkMap, error) {
 				BlockNumber: 0,
 			},
 			RewardsFork_Sabine: Fork{
-				Date:        "1970-01-01", // Enabled from genesis on Hoodi
-				BlockNumber: 0,
+				Date:        "2025-12-19",
+				BlockNumber: 1871733,
 			},
 		}, nil
 	case Chain_PreprodHoodi:
@@ -695,8 +687,8 @@ func (c *Config) GetRewardsSqlForkDates() (ForkMap, error) {
 				BlockNumber: 0,
 			},
 			RewardsFork_Sabine: Fork{
-				Date:        "1970-01-01", // Enabled from genesis on PreprodHoodi
-				BlockNumber: 0,
+				Date:        "2025-12-19",
+				BlockNumber: 1871733,
 			},
 		}, nil
 	case Chain_Mainnet:
@@ -741,8 +733,8 @@ func (c *Config) GetRewardsSqlForkDates() (ForkMap, error) {
 				BlockNumber: 22483225,
 			},
 			RewardsFork_Sabine: Fork{
-				Date:        "2025-12-01", // TODO: Set actual mainnet fork date
-				BlockNumber: 0,            // TODO: Set actual mainnet fork block number
+				Date:        "2026-01-19",
+				BlockNumber: 24274311,
 			},
 		}, nil
 	}
