11dea58b7ac28d4ed72ea372c942e5c44e59b992
Seri Choi <choi.seri65@gmail.com>
2025-12-05 07:43:19 +0900

    materialized + backward compatibility

--- pkg/rewards/5_goldRfaeStakers.go
+++ pkg/rewards/5_goldRfaeStakers.go
@@ -66,7 +66,7 @@ staker_strategy_shares AS (
     sdo.*,
     sss.shares
   FROM staker_delegated_operators sdo
-  JOIN staker_share_snapshots_final sss
+  JOIN staker_share_snapshots sss
   ON
     sdo.staker = sss.staker AND
     sdo.snapshot = sss.snapshot AND
